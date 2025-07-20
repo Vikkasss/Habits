718489b1f3a6f0c60c90f709b543a70ad38bf758
Vikkasss <valedentsova@edu.hse.ru>
2025-07-21 00:37:29 +0300

    modified js file

--- frontend/public/habit.js
+++ frontend/public/habit.js
@@ -1,6 +1,11 @@
 import './calendar'
 import './app'
 
+const tg = window.Telegram.WebApp;
+tg.expand();
+tg.ready();
+
+
 let habits = [];
 let tasks = [];
 let userId = ''
@@ -143,6 +148,54 @@ async function toggleHabitCompletion(event) {
     renderHabits();
     generateCalendar();
 }
+
+function generateCalendar() {
+    const calendar = document.getElementById('calendar-display');
+    calendar.innerHTML = '';
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth();
+    const daysinMonth = new Date(year, month + 1, 0).getDate();
+
+    document.getElementById('current-month').textContent = now.toLocaleString('default', {month: 'long', year: 'numeric'});
+    const firstDay = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < firstDay; i++) {
+        const emptycell = document.createElement('div');
+        emptycell.className = 'calendar-day';
+        calendar.appendChild(emptycell);
+    }
+
+    for (let day = 1; day <= daysinMonth; day++) {
+        const dateStr = `${year}-${String(month + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+        const dayCell = document.createElement('div');
+        dayCell.className = 'calendar-day';
+        const dayNumber = document.createElement('div');
+        dayNumber.className = 'day-number';
+        dayNumber.textContent = day;
+        dayCell.appendChild(dayNumber);
+
+        const hasCompletions = habits.some(habit =>
+                habit.completions?.includes(dateStr)
+        );
+
+        if (hasCompletions) {
+            const activityDot = document.createElement('div');
+            activityDot.className = 'activity-dot';
+            dayCell.appendChild(activityDot);
+        }
+
+        const today = new Date();
+        if (today.getDate() === day &&
+            today.getMonth() === month &&
+            today.getFullYear() === year) {
+            dayCell.classList.add('active');
+        }
+        calendar.appendChild(dayCell);
+    }
+
+}
+
 function isYesterday(dateString) {
     const date = new Date(dateString);
     const yesterday = new Date();
